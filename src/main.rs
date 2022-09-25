ce8a9b20678bdfb4cd7653fe8903caf5214f9a77
amaanq <amaanq12@gmail.com>
2022-09-25 06:02:37 -0400

    ok

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,101 @@
-use std::io::stdin;
-use std::io::{self, Write};
+use std::io::{self, stdin, Write};
 use textplots::{Chart, Plot, Shape};
+
+const GRAVITY: f64 = 9.80665;
 
 fn main() {
     std::env::set_var("LC_ALL", "ru_RU.UTF-8");
 
-    let g = 9.80665; // ускорение свободного падения
-    let mut m: f64; // масса сопротивления
-    let k: f64; // коэффициент сопротивления
-    let c: f64; // начальная скорость
-
     print!("Введите вес мяча в кГ = ");
-    let mut input = String::new();
+    let mut user_input = String::new();
     io::stdout().flush().unwrap();
     io::stdin()
-        .read_line(&mut input)
+        .read_line(&mut user_input)
         .expect("Error reading from STDIN");
-    m = input.trim().parse().expect("Error parsing input");
+    let mut mass = user_input
+        .trim()
+        .parse::<f64>()
+        .expect("Error parsing input");
 
-    m = m / g; // рассчёт массы
+    mass /= GRAVITY; // рассчёт массы
 
-    input.clear();
+    user_input.clear();
     print!("Введите сопротивление воздуха в кГ = ");
     io::stdout().flush().unwrap();
     io::stdin()
-        .read_line(&mut input)
+        .read_line(&mut user_input)
         .expect("Error reading from STDIN");
-    k = input.trim().parse().expect("Error parsing input");
+    let air_resistance_coefficient = user_input
+        .trim()
+        .parse::<f64>()
+        .expect("Error parsing input");
 
-    input.clear();
+    user_input.clear();
     print!("Введите начальную скорость мяча = ");
     io::stdout().flush().unwrap();
     io::stdin()
-        .read_line(&mut input)
+        .read_line(&mut user_input)
         .expect("Error reading from STDIN");
-    c = input.trim().parse().expect("Error parsing input");
+    let initial_speed = user_input
+        .trim()
+        .parse::<f64>()
+        .expect("Error parsing input");
 
     println!("*****************************************");
 
-    let t = f64::sqrt(m / (g * k)) * f64::atan(c * (f64::sqrt(k / (m * g)))); //время
-    let s = m / k
+    let time_air_resistance = f64::sqrt(mass / (GRAVITY * air_resistance_coefficient))
+        * f64::atan(initial_speed * (f64::sqrt(air_resistance_coefficient / (mass * GRAVITY)))); //время
+    let height_air_resistance = mass / air_resistance_coefficient
         * f64::ln(
-            f64::cos(f64::atan(c * (f64::sqrt(k / (m * g)))) - t * (f64::sqrt(g * k / m)))
-                / f64::cos(f64::atan(c * (f64::sqrt(k / (m * g))))),
+            f64::cos(
+                f64::atan(
+                    initial_speed * (f64::sqrt(air_resistance_coefficient / (mass * GRAVITY))),
+                ) - time_air_resistance * (f64::sqrt(GRAVITY * air_resistance_coefficient / mass)),
+            ) / f64::cos(f64::atan(
+                initial_speed * (f64::sqrt(air_resistance_coefficient / (mass * GRAVITY))),
+            )),
         ); //высота
 
-    println!("Время полета = {t} секунд");
-    println!("Наибольшая высота подьёма = {s} метров");
+    println!("Время полета = {time_air_resistance} секунд");
+    println!("Наибольшая высота подьёма = {height_air_resistance} метров");
 
     //Пренебрегая сопротивлением
-    let t1 = c / g; // время
-    let s1 = c * t1 - g * t1 * t1 / 2.0; // высота
+    let time_no_resistance = initial_speed / GRAVITY; // время
+    let height_no_resistance = initial_speed * time_no_resistance
+        - GRAVITY * time_no_resistance * time_no_resistance / 2.0; // высота
 
     println!("\n*****************************************");
     println!("Пренебрегая сопротивлением");
-    println!("Время полета = {t1} секунд");
-    println!("Наибольшая высота подьёма = {s1} метров");
+    println!("Время полета = {time_no_resistance} секунд");
+    println!("Наибольшая высота подьёма = {height_no_resistance} метров");
 
-    stdin().read_line(&mut input).unwrap();
+    stdin().read_line(&mut user_input).unwrap();
 
     println!("График полёта мяча");
 
     Chart::default()
-        .lineplot(&Shape::Continuous(Box::new(|x| x)))
+        .lineplot(&Shape::Continuous(Box::new(|x| {
+            calculate_height(x.into(), initial_speed, air_resistance_coefficient, mass) as f32
+        })))
+        .lineplot(&Shape::Continuous(Box::new(|x| {
+            calculate_height(x.into(), initial_speed, 0.0, mass) as f32
+        })))
         .display();
 }
 
-fn height(t1: f64, s1: f64) {
-
+fn calculate_height(time: f64, initial_speed: f64, coefficient: f64, mass: f64) -> f64 {
+    // returns height
+    if coefficient == 0.0 {
+        initial_speed * time - GRAVITY * time * time / 2.0
+    } else {
+        mass / coefficient
+            * f64::ln(
+                f64::cos(
+                    f64::atan(coefficient * (f64::sqrt(coefficient / (mass * GRAVITY))))
+                        - time * (f64::sqrt(GRAVITY * coefficient / mass)),
+                ) / f64::cos(f64::atan(
+                    coefficient * (f64::sqrt(coefficient / (mass * GRAVITY))),
+                )),
+            )
+    }
 }
